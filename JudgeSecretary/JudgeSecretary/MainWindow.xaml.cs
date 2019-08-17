8757d58c5ffb5fac6889715a3e852a2cdabca722
Пустовитов Андрей <a.pustovitov@robo-med.com>
2019-08-17 22:55:22 +0300

    .

--- JudgeSecretary/JudgeSecretary/MainWindow.xaml.cs
+++ JudgeSecretary/JudgeSecretary/MainWindow.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System.IO;
 using System.Windows;
 using System.Windows.Forms;
+using MessageBox = System.Windows.Forms.MessageBox;
+using Path = System.IO.Path;
 
 namespace JudgeSecretary
 {
@@ -18,13 +20,35 @@ namespace JudgeSecretary
 		{
 			using (var fbd = new FolderBrowserDialog())
 			{
+				fbd.Description = "Выберете папку с приказами";
 				var result = fbd.ShowDialog();
 
 				if (result == System.Windows.Forms.DialogResult.OK && !string.IsNullOrWhiteSpace(fbd.SelectedPath))
 				{
-					string[] files = Directory.GetFiles(fbd.SelectedPath);
+					var files = Directory.GetFiles(fbd.SelectedPath);
 
-					System.Windows.Forms.MessageBox.Show("Files found: " + files.Length, "Message");
+					fbd.Description = "Выберете папку, куда сохранить постановления";
+					result = fbd.ShowDialog();
+
+					if (result == System.Windows.Forms.DialogResult.OK && !string.IsNullOrWhiteSpace(fbd.SelectedPath))
+					{
+						var destinationFolder = fbd.SelectedPath;
+
+						foreach (var file in files)
+						{
+							var destinationFilePath = Path.Combine(destinationFolder, Path.ChangeExtension(Path.GetFileName(file), "txt"));
+							var arguments = $"\"{file}\" /lang Mixed /out \"{destinationFilePath}\" /quit";
+
+							System.Diagnostics.Process process1 = new System.Diagnostics.Process();
+							process1.StartInfo.FileName = FineReaderPathTextBox.Text;
+							process1.StartInfo.Arguments = arguments;
+							process1.Start();
+							process1.WaitForExit();
+							process1.Close();
+						}
+					}
+
+					MessageBox.Show("Готово");
 				}
 			}
 		}
