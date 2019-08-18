8732f16a38b9e7fc8af0153aa428a716dbbda510
Пустовитов Андрей <a.pustovitov@robo-med.com>
2019-08-18 11:55:46 +0300

    .

--- JudgeSecretary/JudgeSecretary/OrderInfo.cs
+++ JudgeSecretary/JudgeSecretary/OrderInfo.cs
@@ -12,6 +12,20 @@
 		{
 			public string FullName { get; set; }
 			public string BirthDate { get; set; }
+			public string BirthPlace { get; set; }
+			public string ResidencePlace { get; set; }
+			public string WorkPlace { get; set; }
+
+			public override bool Equals(object obj)
+			{
+				var info = obj as PersonInfo;
+				return info != null &&
+					   FullName == info.FullName &&
+					   BirthDate == info.BirthDate &&
+					   BirthPlace == info.BirthPlace &&
+					   ResidencePlace == info.ResidencePlace &&
+					   WorkPlace == info.WorkPlace;
+			}
 		}
 	}
 }
--- JudgeSecretary/JudgeSecretary/OrderParser.cs
+++ JudgeSecretary/JudgeSecretary/OrderParser.cs
@@ -8,7 +8,7 @@ namespace JudgeSecretary
 		public OrderInfo Parse(string[] text)
 		{
 			var dateAndCaseNumberRegex = new Regex(@"«(?<Day>\w+)» (?<Month>\w+) (?<Year>\d+)\s+года\s+производство\s+(?<CaseNumber>[\w+\W]+)");
-			var personInfoRegex = new Regex(@"(?<FullName>[а-яА-Я]+\s+[а-яА-Я]+\s+[а-яА-Я]+)\s+(?<BirthDate>\d+\.\d+\.\d+)\s+года\s+рождения");
+			var personInfoRegex = new Regex(@"должника\s*(?<FullName>[а-яА-Я]+\s+[а-яА-Я]+\s+[а-яА-Я]+),*\s*(ИНН \d+)?,*\s+(?<BirthDate>\d+\.\d+\.\d+)\s+года\s+рождения,\s*уроженца (?<BirthPlace>[а-яА-Я\s\.\,]+),\s*место\s*работы:?\s*(?<WorkPlace>[а-яА-Я\s\.\,]+),\s*проживающего\s*по\s*адресу:?\s*(?<ResidencePlace>[\dа-яА-Я\s\.\,\/]+),\s*в\sпользу");
 			var result = new OrderInfo();
 			var persons = new List<OrderInfo.PersonInfo>();
 			foreach (var line in text)
@@ -29,7 +29,14 @@ namespace JudgeSecretary
 					var person = new OrderInfo.PersonInfo();
 					person.FullName = personInfoMatch.Groups["FullName"].Value;
 					person.BirthDate = personInfoMatch.Groups["BirthDate"].Value;
-					persons.Add(person);
+					person.BirthPlace = personInfoMatch.Groups["BirthPlace"].Value;
+					person.ResidencePlace = personInfoMatch.Groups["ResidencePlace"].Value;
+					person.WorkPlace = personInfoMatch.Groups["WorkPlace"].Value;
+
+					if (!persons.Contains(person))
+					{
+						persons.Add(person);
+					}
 				}
 			}
 
